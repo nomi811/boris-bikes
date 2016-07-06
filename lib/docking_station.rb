69bb27f1b92a61a4b492b84d4bc2e32b71fa413d
JulieLev <jkgee1@yahoo.co.uk>
2016-07-06 18:50:19 +0100

    docking_station file release_bike creates bike but spec test is behind with wrong syntax

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,6 @@
 class DockingStation
   def release_bike
-
+    #bike = Bike.new
+    #return bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,5 +6,10 @@ describe DockingStation do
     expect(subject).to respond_to(:release_bike)
   #it { is_expected.to respond_to :release_bike }
   end
+end
 
+describe 'release_bike creates bike' do
+  it 'creates a bike' do
+    expect(release_bike).to eq Bike.new
+  end
 end
