8687aaa859f93e602daac8f0a0a7c28663c12c8b
JulieLev <jkgee1@yahoo.co.uk>
2016-07-06 17:39:26 +0100

    Docking Station has a release_bike and passes rspec test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,9 @@ require 'docking_station'
 
 describe DockingStation do
 
+  it 'responds to release_bike' do
+    expect(subject).to respond_to(:release_bike)
+  #it { is_expected.to respond_to :release_bike }
+  end
+
 end
