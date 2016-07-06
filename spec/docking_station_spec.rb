81be5f44e4d51f76d315ea23302026f009a13f67
JulieLev <jkgee1@yahoo.co.uk>
2016-07-07 00:31:31 +0100

    reference to bike added to docking station and revised docking station spec file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,10 +6,16 @@ describe DockingStation do
     expect(subject).to respond_to(:release_bike)
   #it { is_expected.to respond_to :release_bike }
   end
-end
 
-describe 'release_bike creates bike' do
-  it 'creates a bike' do
-    expect(release_bike).to eq Bike.new
-  end
+  #it 'release_bike creates a bike' do
+    #expect(subject.release_bike).to be_a(Bike)
+  #end
+  ​
+  #it "expects bike to be working" do
+    #expect(subject.release_bike).to be_working
+  #end
+  #it 'releases working bikes' do
+    #bike = subject.release_bike
+    #expect(bike).to be_working
+  #end
 end
