d7ac1a678e242c8965afa4180d986a82054c7d4f
Nomi Vos <nvoswork@gmail.com>
2016-07-07 03:31:57 -0500

    fixed errors that was causing us problems. Now working upto #10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,21 +1,10 @@
 require 'docking_station'
 
 describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
 
-  it 'responds to release_bike' do
-    expect(subject).to respond_to(:release_bike)
-  #it { is_expected.to respond_to :release_bike }
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
-
-  #it 'release_bike creates a bike' do
-    #expect(subject.release_bike).to be_a(Bike)
-  #end
-  ​
-  #it "expects bike to be working" do
-    #expect(subject.release_bike).to be_working
-  #end
-  #it 'releases working bikes' do
-    #bike = subject.release_bike
-    #expect(bike).to be_working
-  #end
 end
