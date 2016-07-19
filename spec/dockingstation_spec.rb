4205767daac0fd0e298edea7ec198ace50e68857
Nomi Vos <nvoswork@gmail.com>
2016-07-19 15:45:59 -0500

    10_making_stations_release_bikes.md

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,15 @@
 require 'dockingstation'
 
 describe DockingStation do
+  subject(:dockingstation) { described_class.new }
+
   it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bikes' do
+    bike = dockingstation.release_bike
+    expect(bike).to be_working
+  end
+
+
+
 end
