26f9cdb8ec502b2c6259cd03765d9772678d5da0
Nomi Vos <nvoswork@gmail.com>
2016-07-19 16:06:40 -0500

    11_using_instance_variables.md

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,13 +3,26 @@ require 'dockingstation'
 describe DockingStation do
   subject(:dockingstation) { described_class.new }
 
-  it { is_expected.to respond_to :release_bike }
-
-  it 'releases working bikes' do
-    bike = dockingstation.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it { is_expected.to respond_to :release_bike }
+    it 'releases working bikes' do
+      bike = dockingstation.release_bike
+      expect(bike).to be_working
+    end
   end
 
-
+  describe '#dock' do
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    it { is_expected.to respond_to(:bike) }
+    it 'sees that a bike has been docked' do
+      bike = Bike.new
+      expect(dockingstation.dock(bike)).to eq bike
+    end
+    it 'returns docked bikes' do
+      bike = Bike.new
+      dockingstation.dock(bike)
+      expect(dockingstation.bike).to eq bike
+    end
+  end
 
 end
